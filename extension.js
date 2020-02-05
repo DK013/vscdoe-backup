51b489c79bdfdb5c32326ab9b6c8cf214f9ebe46
DK013 <chakraborty.deep013@gmail.com>
2020-02-05 18:36:55 +0530

    fn Backup -- file

--- extension.js
+++ extension.js
@@ -1,6 +1,15 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const sh = require('shelljs');
+const Shell = require('node-powershell');
+const fs = require('fs');
+const path = require('path');
+ 
+const ps = new Shell({
+  executionPolicy: 'Bypass',
+  noProfile: true
+});
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -19,9 +28,33 @@ function activate(context) {
 	// The commandId parameter must match the command field in package.json
 	let command1 = vscode.commands.registerCommand('extension.backup', function () {
 		// The code you place here will be executed every time your command is executed
-
+		if(process.platform === "win32") {
+			ps.addCommand('code --list-extensions | % { "code --install-extension $_" }');
+			ps.invoke()
+			.then(output => {
+				var folderPath = path.join(process.env.APPDATA, 'Code\\User');
+				fs.writeFile(path.join(folderPath, 'plugins.txt'), output, error => {
+					if(error) {
+						console.log(error);
+						vscode.window.showErrorMessage('Failed to create backup file');
+					}
+					else {
+						vscode.window.showInformationMessage('Backup File Created');
+					}
+				});
+			})
+			.catch(err => {
+				console.log(err);
+				vscode.window.showErrorMessage('Failed to get extensions list');
+			});
+		}
+		else {
+			sh.exec('code --list-extensions | xargs -L 1 echo code --install-extension', (code, output) => {
+				vscode.window.showInformationMessage(output);
+			});
+		}
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Backup Command Fired');
+		
 	});
 
 	let command2 = vscode.commands.registerCommand('extension.restore', function () {
